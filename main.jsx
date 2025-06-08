5a5656263246da6c7e3f3200f99e922fc52f1272
BritishErrorCoins <jrenaud001@gmail.com>
2025-06-08 22:03:16 +0100

    Add it all

--- main.jsx
+++ main.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MyCollection from './components/MyCollection';
+
 import MyWantlist from './components/MyWantlist';
 import SoldCoins from './components/SoldCoins';
 import BrowseCoins from './components/BrowseCoins';
+import MyCollection from './components/MyCollection';
+
 import './styles/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<BrowseCoins />} />
+        <Route path="/" element={<BrowseCoins />} /> {/* Default page */}
         <Route path="/collection" element={<MyCollection />} />
-        <Route path="/wantlist" element={<MyWantlist />} />
+        <Route path="/browse" element={<BrowseCoins />} />
         <Route path="/sold" element={<SoldCoins />} />
+        <Route path="/wantlist" element={<MyWantlist />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ utils/exportUtils.js
@@ -0,0 +1,16 @@
+import { saveAs } from "file-saver";
+
+export function exportToCSV(data, filename = "export.csv") {
+  if (!data || !data.length) return;
+
+  const headers = Object.keys(data[0]);
+  const csv = [
+    headers.join(","),
+    ...data.map(row =>
+      headers.map(field => `"${(row[field] ?? "").toString().replace(/"/g, '""')}"`).join(",")
+    )
+  ].join("\r\n");
+
+  const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  saveAs(blob, `${filename}.csv`);
+}
