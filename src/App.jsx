f4dcea6f62208a056b862c22194c0930d9a262ab
BritishErrorCoins <jrenaud001@gmail.com>
2025-06-09 06:23:44 +0100

    Update: 2025-06-09 06:23:44

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-// src/App.jsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import MyCollection from "./components/MyCollection";
-import MyWantlist from "./components/MyWantlist";
-import SoldCoins from "./components/SoldCoins";
-import BrowseCoins from "./components/BrowseCoins";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import BrowseCoins from './components/BrowseCoins';
+import MyCollection from './components/MyCollection';
+import MyWantlist from './components/MyWantlist';
+import SoldCoins from './components/SoldCoins';
 
 function App() {
   return (
     <div className="app-container">
       <Header />
-      <main>
+      <main className="content">
         <Routes>
           <Route path="/" element={<BrowseCoins />} />
           <Route path="/collection" element={<MyCollection />} />
